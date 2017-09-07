febdacfebed75fc92d3af06fc483be51f6b808f0
NxDon <645104928@qq.com>
2017-09-07 21:27:41 +0800

    finish routers

--- config/default.js
+++ config/default.js
@@ -1,8 +1,8 @@
 module.exports = {
-    port:4000,
+    port:3000,
     session:{
         secret:'myblog',
-        key:'my simple blog',
+        key:'myblog',
         maxAge:2592000000
     },
     mongodb:'mongodb://localhost:27017/myblog'
